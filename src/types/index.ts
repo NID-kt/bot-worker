77c23a599bef4a4596a4a73e9f6af9895e596481
Yuto Terada <lugongdao07@gmail.com>
2024-08-08 16:11:41 +0900

    feat: add ScheduledEvent interface to types

--- src/types/index.ts
+++ src/types/index.ts
@@ -20,3 +20,14 @@ export interface QueryCache {
   reactionAgentEmojis: ReactionAgentEmoji[];
   commands: Command[];
 }
+
+export interface ScheduledEvent {
+  id: string;
+  name: string;
+  description?: string | undefined | null;
+  starttime: Date;
+  endtime?: Date | undefined | null;
+  creatorid?: string | undefined | null;
+  location?: string | undefined | null;
+  recurrence?: string | undefined | null;
+}
